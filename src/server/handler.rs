3e653dd0ea584187bd5a673d6e4dcf61db689d2b
ErnestRC <ernest@unstable.build>
2016-12-18 15:54:39 -0800

    wip sonic handler

--- src/server/handler.rs
+++ src/server/handler.rs
@@ -12,22 +12,45 @@ pub struct SonicHandler<'b, H>
     handler: H,
     rbuffer: &'b mut ByteBuffer,
     wbuffer: &'b mut ByteBuffer,
+    is_readable: bool,
+    is_writable: bool,
+    complete: bool,
 }
 
 impl<'b, H> SonicHandler<'b, H>
     where H: Handler<In = Result<SonicMessage>, Out = SonicMessage>,
 {
-    pub fn new(
-        handler: H,
-        rbuffer: &'b mut ByteBuffer,
-        wbuffer: &'b mut ByteBuffer
-    ) -> SonicHandler<'b, H> {
-        SonicHandler {
-            handler: handler,
-            rbuffer: rbuffer,
-            wbuffer: wbuffer,
+    // pub fn new(
+    //    handler: H,
+    //    rbuffer: &'b mut ByteBuffer,
+    //    wbuffer: &'b mut ByteBuffer
+    // -> SonicHandler<'b, H> {
+    //    SonicHandler {
+    //        handler: handler,
+    //        rbuffer: rbuffer,
+    //        wbuffer: wbuffer,
+    //    }
+    //
+}
+
+#[macro_export]
+macro_rules! mtry {
+    ($e:expr, $sel:expr) => {{
+        match $e {
+            Ok(_) => {},
+            Err(e) => {
+                let err = format!("unexpected SonicHandler err: {}", e); 
+                error!("{}", err);
+                $sel.wbuffer.clear();
+                $sel.complete = true;
+                SonicMessage::complete(Err(err.into()), 
+                                       "".to_owned()).to_buffer($sel.wbuffer).unwrap();
+
+                // TODO
+                $sel.try_write().unwrap();
+            }
         }
-    }
+    }}
 }
 
 // TODO should read/write until EAGAIN and keep state of readable/writable
@@ -37,8 +60,6 @@ impl<'b, H> Handler for SonicHandler<'b, H>
     type In = MuxEvent;
     type Out = MuxCmd;
 
-    fn update(&mut self, epfd: EpollFd) {}
-
     fn ready(&mut self, event: MuxEvent) -> MuxCmd {
         let fd = event.fd;
         let kind = event.kind;
@@ -47,20 +68,24 @@ impl<'b, H> Handler for SonicHandler<'b, H>
             return MuxCmd::Close;
         }
 
+        if kind.contains(EPOLLIN) {
+            self.is_readable = true;
+        }
+
+        if kind.contains(EPOLLOUT) {
+            self.is_writable = true;
+        }
+
         if kind.contains(EPOLLERR) {
             let err = format!("fd={}: EPOLERR", fd);
             error!("{}", err);
             let res = self.handler.ready(Err(err.into()));
-            let writer: &mut [u8] = self.wbuffer.mut_slice(4);
-            // TODO res.to_writer(&mut writer).unwrap();
-        }
 
-        if kind.contains(EPOLLIN) {
+            if let &SonicMessage::StreamCompleted(_, _) = &res {
+                self.complete = true;
+            }
 
-        }
-
-        if kind.contains(EPOLLOUT) {
-
+            mtry!(res.to_buffer(self.wbuffer), self);
         }
 
         MuxCmd::Keep
